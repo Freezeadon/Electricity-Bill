3604ba01c0b7f0393c9ea4b60ed40d5ba0b8e0b0
Freezeadon <52727355+Freezeadon@users.noreply.github.com>
2023-11-21 17:27:39 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -5,6 +5,12 @@ public class GUI extends JFrame {
 
     GUI(String title){
         super(title);
+        try {
+            ImageIcon img = new ImageIcon("src/Lighting.png");
+            setIconImage(img.getImage());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         getContentPane().setBackground(Color.pink);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(Main.EXIT_ON_CLOSE);
