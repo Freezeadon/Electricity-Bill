3396dfb8c418552e976e561c79796e165b7a2bfc
hamidsarim <80725988+hamidsarim@users.noreply.github.com>
2023-11-26 03:10:08 -0500

    Update GoldPlan.java

--- GoldPlan.java
+++ GoldPlan.java
@@ -1,23 +1,35 @@
-package main;
+public class GoldPlan {
+    private static final float OFF_PEAK_PRICE = 15.3f;
+    private static final float MID_PEAK_PRICE = 20f;
+    private static final float ON_PEAK_PRICE = 22.5f;
+    private static final float DELIVERY_PRICE = 20f;
+    private static final float REGULATORY_CHARGE = 15f;
 
-public class GoldPlan extends ElectricityPlan {
+    // Getter methods for the prices
+    public static float getOffPeakPrice() {
+        return OFF_PEAK_PRICE;
+    }
 
-	@Override
-	public void calculateTotalCost(int offPeakTime, int midPeakTime, int onPeakTime) {
-		
-		offPeakPrice = 15.3;
-		midPeakPrice = 20;
-		onPeakPrice = 22.5;
+    public static float getMidPeakPrice() {
+        return MID_PEAK_PRICE;
+    }
 
-		deliveryPrice = 20;
-		regulatoryCharge = 15;
-		
-		float totalTime = offPeakTime + midPeakTime + onPeakTime;
+    public static float getOnPeakPrice() {
+        return ON_PEAK_PRICE;
+    }
 
-		totalCost = (float) ((offPeakTime * offPeakPrice) + (midPeakTime * midPeakPrice) + (onPeakTime * onPeakPrice));
-		
-		hst = (float) (totalCost + overCost * 0.13);
-		
-		total = totalCost + hst;
-	}
+    public static float getDeliveryPrice() {
+        return DELIVERY_PRICE;
+    }
+
+    public static float getRegulatoryCharge() {
+        return REGULATORY_CHARGE;
+    }
+
+    // Method to calculate cost for Gold plan
+    public static float calculateCost(int offPeakTime, int midPeakTime, int onPeakTime) {
+        float totalCost = (offPeakTime * OFF_PEAK_PRICE) + (midPeakTime * MID_PEAK_PRICE) + (onPeakTime * ON_PEAK_PRICE);
+        float hst = totalCost * 0.13f;
+        return totalCost + DELIVERY_PRICE + REGULATORY_CHARGE + hst;
+    }
 }
